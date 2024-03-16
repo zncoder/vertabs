755301ccf48a5bddc4178ebbf6890384f6f37a39
Bin Zhang <zncoder@gmail.com>
2024-03-15 22:27:56 -0700

    switch to archive.org

--- action.js
+++ action.js
@@ -11,5 +11,6 @@ document.querySelector('#undo-btn').onclick = undoTab
 document.querySelector('#prevtab-btn').onclick = prevTab
 document.querySelector('#group-btn').onclick = groupTabs
 document.querySelector('#zoom-btn').onclick = zoomPage
-document.querySelector('#archiveorg-btn').onclick = archivePhPage
+// document.querySelector('#archiveorg-btn').onclick = archivePhPage
+document.querySelector('#archiveorg-btn').onclick = archiveOrgPage
 document.querySelector('#paste-btn').onclick = newTabWithUrl
