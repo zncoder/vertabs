5d3a41d8c61b4ef75f9c1b7b096bf7ad2d21319f
Bin Zhang <zncoder@gmail.com>
2024-09-02 13:21:43 -0700

    close tab and close window

--- background.js
+++ background.js
@@ -1,1 +1,34 @@
-browser.browserAction.onClicked.addListener(() => browser.sidebarAction.toggle())
+function handleMenu(info, tab) {
+	switch (info.menuItemId) {
+	case 'close-win':
+		closeCurWin()
+		break
+	case 'close-tab':
+		closeCurTab()
+		break
+	}
+}
+
+async function closeCurWin() {
+	let win = await browser.windows.getCurrent()
+	await browser.windows.remove(win.id)
+}
+
+async function closeCurTab() {
+	let [tab] = await browser.tabs.query({active: true, currentWindow: true})
+	await browser.tabs.remove(tab.id)
+}
+
+browser.browserAction.onClicked.addListener(browser.sidebarAction.toggle)
+
+browser.menus.create({
+  id: 'close-win',
+  title: 'Close window',
+  contexts: ['browser_action'],
+})
+browser.menus.create({
+  id: 'close-tab',
+  title: 'Close tab',
+  contexts: ['browser_action'],
+})
+browser.menus.onClicked.addListener(handleMenu)
